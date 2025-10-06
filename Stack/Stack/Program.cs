ab90157de82333a0afa8f47bf21630001067836e
MoOsA5 <moosasumer01@gmail.com>
2025-10-06 20:36:22 +0300

    Stack

---
+++ Stack/Stack/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack s1 = new Stack();
+            s1.Push(10);
+            s1.Push(20);
+            s1.Push(30);
+
+            Console.WriteLine($"Top : {s1.Top}");
+
+            Console.WriteLine(s1.Pop());
+            Console.WriteLine(s1.Pop());
+            Console.WriteLine(s1.Pop());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Stack/Stack/Stack.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+namespace Stack
+{
+    class Stack
+    {
+        const int capacity = 10;
+        int[] data = new int[capacity];
+        int top;
+
+        public Stack()
+        {
+            top = 0;
+        }
+        public void Push(int value)
+        {
+            if (top < capacity)
+            {
+                data[top] = value;
+                top++;
+            }
+        }
+        public int Pop()
+        {
+            if (!IsEmpty)
+            {
+                top--;
+                return data[top];
+            }
+            else return 0;
+        }
+        private bool IsEmpty => top == 0;
+        public int Top => top;
+    }
+}

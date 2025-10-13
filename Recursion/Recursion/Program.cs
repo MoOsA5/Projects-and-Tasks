b40378725f562cbe7754fd31369633c0a71f277c
MoOsA5 <moosasumer01@gmail.com>
2025-10-13 20:51:27 +0300

    Recursion Tasks

---
+++ Recursion/Recursion/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Recursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>(){ 3, 10, 5, 7, 20, 15};
+            List<int> list2 = new List<int>() { 1, 3, 5, 7, 9 };
+            //List<int> sortedList = Solutions.QuickSort(list);
+
+            Console.WriteLine($"Lagest Number is : {Solutions.MaxNumber(list)}");
+            Console.WriteLine($"Smallest Number is : {Solutions.MinNumber(list)}");
+            Console.WriteLine($"Sum of numbers : {Solutions.Sum(list)}");
+            Solutions.BinarySearch(list2,7);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Recursion/Recursion/Solutions.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Recursion
+{
+    class Solutions
+    {
+        public static int MaxNumber(List<int> list)
+        {
+            int n = list.Count;
+            //Base Case
+            if (n == 1)
+            {
+                return list[0];
+            }
+            //Recursion Case
+            int mid = n / 2;
+            int left = MaxNumber(list.GetRange(0, mid));
+            int right = MaxNumber(list.GetRange(mid, n-mid));
+
+            return left > right ? left : right;
+        }
+
+        public static int MinNumber(List<int> list)
+        {
+            int n = list.Count;
+            //Base Case
+            if (n == 1)
+            {
+                return list[0];
+            }
+            //Recursion Case
+            int mid = n / 2;
+            int left = MinNumber(list.GetRange(0, mid));
+            int right = MinNumber(list.GetRange(mid, n - mid));
+
+            return left < right ? left : right;
+        }
+
+        public static int Sum(List<int> list)
+        {
+            int n = list.Count;
+            //Base Case
+            if (n == 1)
+            {
+                return list[0];
+            }
+            //Recursion Case
+            int mid = n / 2;
+            int left = Sum(list.GetRange(0, mid));
+            int right = Sum(list.GetRange(mid, n - mid));
+       
+            return left + right;
+        }
+
+        public static void BinarySearch(List<int> list,int target)
+        {
+            int low = 0, high = list.Count - 1;
+            while (low <= high)
+            {
+                int mid = low + (high - low) / 2;
+
+                if (list[mid] == target)
+                    Console.WriteLine($"The target {target} is at index {mid}");
+
+                if (list[mid] < target)
+                    low = mid + 1;
+
+                else
+                    high = mid - 1;
+            }
+        }
+
+        public static List<int> QuickSort(List<int> list)
+        {
+            int n = list.Count;
+            List<int> sortedList = new List<int>();
+            //Base Case
+            if (n == 1)
+            {
+                return list;
+            }
+            //Recursion Case
+            int pivot = n / 2;
+            List<int> left = QuickSort(list.GetRange(0, pivot));
+            List<int> right = QuickSort(list.GetRange(pivot, n - pivot));
+
+            sortedList.Add(pivot);
+            return sortedList;
+        }
+
+    }
+}
